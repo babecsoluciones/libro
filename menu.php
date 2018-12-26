c57ffc73f6bd7ce96a409eace2e6c9d5de27f7da
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-26 17:28:49 -0600

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,14 +2,14 @@
 $url = "http://historiadelaradio.com/libro/";
 ?>
 <ul>
-										<li><a href="<?=$url?>inicio">Inicio</a></li>
-										<li><a href="<?=$url?>historia">Historia</a></li>
+										<li><a href="<?=$url?>">Inicio</a></li>
+										<li><a href="index.php?seccion=historia">Historia</a></li>
 										<!--<li><a href="index.php?seccion=historia">Historia</a></li>-->
 										<li>
 											<span class="opener">Juegos</span>
 											<ul>
-												<li><a href="<?=$url?>puzzle">Rompecabezas</a></li>
-												<li><a href="<?=$url?>laberinto">Laberinto</a></li>
+												<li><a href="index.php?seccion=puzzle">Rompecabezas</a></li>
+												<li><a href="index.php?seccion=laberinto">Laberinto</a></li>
 											</ul>
 										</li>
 									</ul>
