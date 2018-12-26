6862bf248fa98a67a86454d74e1c3f4a7ccccce2
Mario Ernesto Basurto Medrano <mario@d80.mx>
2018-12-26 17:26:57 -0600

    ...

--- menu.php
+++ menu.php
@@ -1,12 +1,15 @@
+<?
+$url = "http://historiadelaradio.com/libro/";
+?>
 <ul>
-										<li><a href="index.php">Inicio</a></li>
-										<li><a href="index.php?seccion=historia">Historia</a></li>
+										<li><a href="<?=$url?>inicio">Inicio</a></li>
+										<li><a href="<?=$url?>historia">Historia</a></li>
 										<!--<li><a href="index.php?seccion=historia">Historia</a></li>-->
 										<li>
 											<span class="opener">Juegos</span>
 											<ul>
-												<li><a href="index.php?seccion=puzzle">Rompecabezas</a></li>
-												<li><a href="index.php?seccion=laberinto">Laberinto</a></li>
+												<li><a href="<?=$url?>puzzle">Rompecabezas</a></li>
+												<li><a href="<?=$url?>laberinto">Laberinto</a></li>
 											</ul>
 										</li>
 									</ul>
